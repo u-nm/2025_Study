ef858b51fd0aebb5352328571d43f5f63b0613c9
u-nm <8765431@naver.com>
2025-01-03 17:34:43 +0900

    newwwww

---
+++ javascript/To-Do/todo.js
@@ -0,0 +1,10 @@
+
+
+// qus수부터 설정
+const todoInput = document.querySelector('todo-txt');
+const todoList = document.querySelector('todo-list');
+const addTodo = document.querySelector('add-todo');
+const delTodo = document.querySelector('del-todo');
+
+
+
