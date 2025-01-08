72a5d630e641f11a76ab3215feae645c841ca099
u-nm <8765431@naver.com>
2025-01-08 17:58:02 +0900

    img

--- javascript/Caulator/caulator.js
+++ javascript/Caulator/caulator.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let currentInput = ""; // 최근 입력값
     let operator = ""; // 계산식
     let previousInput = ""; // 이전 입력값
+    let activeOperator = null;
 
     buttons.forEach(button => { // buttons 에 button 이 순서대로 배열로 드러가잇슴
 
@@ -14,9 +15,20 @@ document.addEventListener('DOMContentLoaded', () => {
         button.addEventListener('click', () => {
             const value = button.textContent;
 
-            if(button.className == 'number') {
+            if(button.classList.contains('number')) {
                 currentInput += value;
                 display.textContent = currentInput;
+            } else if(button.classList.contains('decimal')) {
+                if(!currentInput.includes('.')) {
+                    currentInput += value;
+                    display.textContent = currentInput;
+                }
+            } else if(button.classList.contains('operator')) {
+                if(currentInput !== '') {
+                    previousInput = currentInput;
+                    currentInput = '';
+                    operator = value;
+                }
             }
             
         })
